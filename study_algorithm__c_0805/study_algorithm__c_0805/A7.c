80127b6a4d2fa9c2a3c78bac84f38bf62806657a
Youngho Kim <graycoral@nate.com>
2019-08-14 17:46:33 +0900

    A7: [TST]숫자 카운팅 - 무한 루프 발생

---
+++ study_algorithm__c_0805/study_algorithm__c_0805/A7.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#define MAXN	((int)2e5+10)
+
+int N, M;
+int q[MAXN];
+
+void input()
+{
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &q[i]);
+	}
+}
+
+int downBS(int s, int e, int val)
+{
+	int m = (s + e) / 2;
+	static int retVal = -1;
+
+	if (s > e)	return retVal;
+
+	if (q[m] == val) {
+		retVal = m;
+		downBS(s, m - 1, val);
+	}
+	else if (q[m] > val) { return upBS(s, m - 1, val); }
+	else { return upBS(m + 1, e, val); }
+
+	return retVal;
+}
+
+int upBS(int s, int e, int val)
+{
+	int m = (s + e) / 2;
+	static int retVal = -1;
+
+	if (s > e)	return retVal;
+
+	if (q[m] == val) {
+		retVal = m;
+		upBS(s, m + 1, val);
+	}
+	else if (q[m] > val) { return upBS(s, m - 1, val); }
+	else { return upBS(m + 1, e, val); }
+
+	return retVal;
+}
+
+
+
+int sol()
+{
+	int retVal = -1;
+	int M;
+	scanf("%d", &M);
+
+	while (M--) {
+		int tmp;
+		scanf("%d", &tmp);
+		retVal = upBS(0, N, tmp) - downBS(0, N, tmp);
+	}
+
+	return retVal;
+}
+
+int main()
+{
+	int ans = 0;
+
+	input();
+	ans = sol();
+	printf("%d", ans);
+
+	return 0;
+}
--- study_algorithm__c_0805/study_algorithm__c_0805/소스.cpp
+++ study_algorithm__c_0805/study_algorithm__c_0805/소스.cpp
@@ -10,11 +10,64 @@ void input()
 	for (int i = 0; i < N; i++) {
 		scanf("%d", &q[i]);
 	}
-
 }
 
+int upBS(int s, int e, int val)
+{
+	int m = (s + e) / 2;
+	static int retVal = -1;	 
+
+	if (s > e)	return retVal;
+
+	if (q[m] == val) {
+		retVal = m;
+		upBS(s, m + 1, val);
+	}
+	else if (q[m] > val) { return upBS(s, m - 1, val);	}
+	else { return upBS(m + 1, e, val); }
+		
+	return retVal;
+}
+
+int downBS(int s, int e, int val)
+{
+	int m = (s + e) / 2;
+	static int retVal = -1;
+
+	if (s > e)	return retVal;
+
+	if (q[m] == val) {
+		retVal = m;
+		downBS(s, m - 1, val);
+	}
+	else if (q[m] > val) { return upBS(s, m - 1, val); }
+	else { return upBS(m + 1, e, val); }
+
+	return retVal;
+}
+
+int sol()
+{
+	int retVal = -1;
+	int M;
+	scanf("%d", &M);
+
+	while (M--) {
+		int tmp;
+		scanf("%d", &tmp);
+		retVal = upBS(0, N, tmp) - downBS(0, N, tmp);
+	}
+
+	return retVal;
+}
 
 int main()
 {
+	int ans = 0;
+	
+	input();
+	ans = sol();
+	printf("%d", ans);
+
 	return 0;
 }
